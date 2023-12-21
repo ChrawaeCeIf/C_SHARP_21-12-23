410e61a3be4c5986c14383cb8f59a6f21e49ca8a
Артём Глушков <artyom.glushkov.2000@bk.ru>
2023-12-21 22:02:32 +0300

    Complete first task

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет является второе число квадратом второго.
+
+Console.Write("Enter A Number 1");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter A Number 2");
+int b2 = int.Parse(Console.ReadLine()!);
+if (b1 * b1 == b2) 
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
