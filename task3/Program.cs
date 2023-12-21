b215e8eb419ee64244e5712514b3522765bc92d4
Артём Глушков <artyom.glushkov.2000@bk.ru>
2023-12-21 22:15:16 +0300

    Complete third task

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a number: ");
+int a = int.Parse(Console.ReadLine()!);
+int firstDigit;
+int lastDigit;
+int sum;
+
+firstDigit = a / 100;
+lastDigit = a % 10;
+sum = firstDigit + lastDigit;
+Console.WriteLine(sum);
+
